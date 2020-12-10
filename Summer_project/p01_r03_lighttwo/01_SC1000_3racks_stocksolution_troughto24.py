b5f3f07f2bbbeaa8d3253a0cd8acba059199d162
Mingxuan Jiang <40763563+PanoptoSalad@users.noreply.github.com>
2020-12-11 04:38:11 +0800

    Additional comments, no code changed

--- Summer_project/p01_r03_lighttwo/01_SC1000_3racks_stocksolution_troughto24.py
+++ Summer_project/p01_r03_lighttwo/01_SC1000_3racks_stocksolution_troughto24.py
@@ -4,10 +4,13 @@ robot.head_speed(x=18000, y=18000, z=5000, a=700, b=700)
 
 
 class Vector(object):
+    '''Storing and manipulating two-dimensional data'''
     def tolist(self):
+        '''Convert vector object to list'''
         return list(self.input_list)
 
     def astype(self, input_type):
+        '''Cast a vector to a specific type'''
         if input_type == int:
             return Vector([int(float(x)) for x in self.input_list])
         return Vector([input_type(x) for x in self.input_list])
@@ -17,10 +20,14 @@ class Vector(object):
 
 
 class DataFrame(object):
+    '''DataFrame class obtained from a dictionary'''
+    
     def __len__(self):
+        '''Get length of dataframe'''
         return self.length
 
     def __getitem__(self, value):
+        '''Get a vector from dataframe based on value input'''
         return Vector(self.dict_input[value])
 
     def __init__(self, dict_input, length):
@@ -30,19 +37,20 @@ class DataFrame(object):
 
 # Function that reads a csv file correctly without having to import anything (issues with molport). Uses 2 classes, Vector and DataFrame
 def read_csv(input_file):
+    '''reads a csv file, converts it into a dataframe'''
     lines = open(input_file).readlines()
-    header = lines[0].rstrip().split(",")
-    out_d = {}
+    header = lines[0].rstrip().split(",") # get header from csv file by removing commas on first line
+    out_d = {} # initiliase empty dictionary to add headers
     for head in header:
-        out_d[head] = []
+        out_d[head] = [] # initialise empty list to append values
     for line in lines[1:]:
-        spl_line = line.rstrip().split(",")
+        spl_line = line.rstrip().split(",") # remove commas from other lines
         for i, head in enumerate(header):
-            out_d[head].append(spl_line[i])
-    df = DataFrame(out_d, len(lines[1:]))
+            out_d[head].append(spl_line[i]) # values have been appended to list, as part of dictionary
+    df = DataFrame(out_d, len(lines[1:])) # convert dictionary to dataframe
     return df
 
-# CSV file data
+# CSV file data to dataframe
 solvent_df = read_csv(r"C:\Users\opentrons\protocols\GitHub_repos\OT1-coding\Summer_project\p01_r03_lighttwo\csv\050919_JMX_56_reactions\reaction_conditions.csv")
 rack_one_df = read_csv(
     r"C:\Users\opentrons\protocols\GitHub_repos\OT1-coding\Summer_project\p01_r03_lighttwo\csv\050919_JMX_56_reactions\rack1.csv")
@@ -71,6 +79,7 @@ p1000 = instruments.Pipette(
     min_volume=30,
     channels=1,
 )
+# location of the reaction vessels/equipment
 rack_ID_header = "Rack ID"
 id_header = "Reaction parameters type"
 solvent = "Reaction solvent"
@@ -83,26 +92,26 @@ destination_location_header = "Location"
 volume_stock_header = "Volume to dispense (exp) at 0.8M"
 
 def stock_solution_reactant(reactants_df, solvent_df):
-
+    '''Tells opentrons how to prepare stock solution'''
     for i, x in enumerate(solvent_df[id_header].tolist()):
         if x == solvent:
-            solvent_location = solvent_df[location_header].tolist()[i]
+            solvent_location = solvent_df[location_header].tolist()[i] # tell OT1 where solvent is
 
-    p1000.pick_up_tip()
+    p1000.pick_up_tip() # OT1 gets ready to pipette
 
     for i, x in enumerate(reactants_df[destination_location_header].tolist()):
-        destination_location = x
+        destination_location = x # tell OT1 where to dispense the solvent to, to make reactants stock
         vol_to_dispense = [reactants_df[volume_stock_header].tolist()[i]]
         reactants_id = reactants_df[rack_ID_header].tolist()[i]
         if reactants_id == "":
             print ('null')
-            break
+            break # exception handling, if empty rows are detected
         # print (rack_ID_header, reactants_id, vol_to_dispense)
-        if reactants_id == rack_1:
+        if reactants_id == rack_1: # read rack 1 conditions
             #print ('rack1')
             if vol_to_dispense != 0:
-                p1000.transfer(vol_to_dispense, source_trough4row.wells(solvent_location),
-                               rack_stock_reactants_1.wells(destination_location).top(-5), new_tip='never', air_gap=10)
+                p1000.transfer(vol_to_dispense, source_trough4row.wells(solvent_location), # where to get solvent from
+                               rack_stock_reactants_1.wells(destination_location).top(-5), new_tip='never', air_gap=10) # where to dispense solvent to
         if reactants_id == rack_2:
             #print ('rack2')
             if vol_to_dispense != 0:
@@ -113,11 +122,6 @@ def stock_solution_reactant(reactants_df, solvent_df):
             if vol_to_dispense != 0:
                 p1000.transfer(vol_to_dispense, source_trough4row.wells(solvent_location),
                                rack_stock_reactants_3.wells(destination_location).top(-5), new_tip='never', air_gap=10)
-        #if reactants_id == rack_4:
-            #print ('rack4')
-            #if vol_to_dispense != 0:
-                #p1000.transfer(vol_to_dispense, source_trough4row.wells(solvent_location),
-                               #rack_stock_reactants_4.wells(destination_location).top(-5), new_tip='never', air_gap=10)
     p1000.drop_tip()
 
 
