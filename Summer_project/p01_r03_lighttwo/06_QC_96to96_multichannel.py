fa7af9c2e77f5e0333ee58239151c24a0c208058
Mingxuan Jiang <40763563+PanoptoSalad@users.noreply.github.com>
2020-12-11 15:25:45 +0800

    Update 06_QC_96to96_multichannel.py
    comments

--- Summer_project/p01_r03_lighttwo/06_QC_96to96_multichannel.py
+++ Summer_project/p01_r03_lighttwo/06_QC_96to96_multichannel.py
@@ -7,7 +7,7 @@ robot.head_speed(x=18000,  y=18000,  z=3000, a=400, b=400)
 tiprack_300 = containers.load("tiprack-300ul", "D3")
 source_trough4row = containers.load("trough-12row", "C2")
 reaction_rack = containers.load("Para_dox_96_short", "C1")
-destination_QC = containers.load("96-PCR-flat", "B1", "QC")
+destination_QC = containers.load("96-PCR-flat", "B1", "QC") # QC plate
 trash = containers.load("point", "B3")
 
 #Pipettes SetUp
@@ -20,17 +20,17 @@ p300_multi  = instruments.Pipette(
     min_volume=0,
     channels=8,
 )
-volume_to_dispense = 10
-location_QC_solvent = 'A5'
-volume_QC_solvent = 50
-number_rows = 10
+volume_to_dispense = 10 # how much reaction mixture to QC
+location_QC_solvent = 'A5' # Where on the solvent trough is the solvent
+volume_QC_solvent = 50 # how much solvent per QC well to add
+number_rows = 10 # how many times the 8 channel pipette has to dispense QC solvent
 number_rows = number_rows - 1
 
-source_location = [well.bottom(4) for well in reaction_rack.rows(0, to=number_rows)]
-destination_location = [well.bottom(1) for well in destination_QC.rows(0, to=number_rows)]
-destination_QC_solvent = [x.top() for x in destination_QC.rows(0,to=number_rows)]
-p300_multi.distribute(volume_QC_solvent, source_trough4row.wells(location_QC_solvent), destination_QC_solvent)
-p300_multi.transfer(volume_to_dispense, source_location, destination_location, blow_out=True, new_tip = 'always')
+source_location = [well.bottom(4) for well in reaction_rack.rows(0, to=number_rows)] # where to extract reaction mixture from
+destination_location = [well.bottom(1) for well in destination_QC.rows(0, to=number_rows)] # where to dispense reaction mixture to
+destination_QC_solvent = [x.top() for x in destination_QC.rows(0,to=number_rows)] # where to dispense the QC solvent to
+p300_multi.distribute(volume_QC_solvent, source_trough4row.wells(location_QC_solvent), destination_QC_solvent) # dispense QC solvent
+p300_multi.transfer(volume_to_dispense, source_location, destination_location, blow_out=True, new_tip = 'always') # dispense reaction mixture
 robot.home()
 
 for c in robot.commands():
